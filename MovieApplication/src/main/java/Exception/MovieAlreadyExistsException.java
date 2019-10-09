fbaa74fb099be3283fc5f246b019e7382bd0d79f
atul <atulk19966@gmail.com>
2019-10-09 11:43:36 +0530

    Exception

---
+++ MovieApplication/src/main/java/Exception/MovieAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+
+public class MovieAlreadyExistsException extends Exception {
+
+    public MovieAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ MovieApplication/src/main/java/Exception/MovieNotFoundException.java
@@ -0,0 +1,8 @@
+package Exception;
+
+public class MovieNotFoundException extends Exception{
+
+    public MovieNotFoundException(String message) {
+        super(message);
+    }
+}
